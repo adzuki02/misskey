04674adb72e5d1b3157c753aff008b3a67a3a739
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-05-27 23:06:14 +0900

    HTL・STLで自分のフォロワー限定の投稿へのリプライが表示されるように

--- packages/backend/src/server/api/endpoints/notes/timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/timeline.ts
@@ -118,7 +118,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 					}
 
 					if (note.reply && note.reply.visibility === 'followers') {
-						if (!Object.hasOwn(followings, note.reply.userId)) return false;
+						if (note.reply.userId !== me.id && !Object.hasOwn(followings, note.reply.userId)) return false;
 					}
 
 					return true;
