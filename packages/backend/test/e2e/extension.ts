db7c6a62482058250d28b0794faf9f7e30c270ac
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-06-28 02:14:28 +0900

    fixed backend e2e test

--- packages/backend/test/e2e/extension.ts
+++ packages/backend/test/e2e/extension.ts
@@ -20,7 +20,7 @@ describe('独自拡張', () => {
 		bob = await signup({ username: 'bob' });
 		remoteUser = await signup({ username: 'remoteUser', host: genHost() });
 		proxy = await signup({ username: 'proxy' });
-		await api('/admin/update-meta', { proxyAccountId: proxy.id }, alice);
+		await api('admin/update-meta', { proxyAccountId: proxy.id }, alice);
 	}, 1000 * 60 * 2);
 
 	describe('連合に関する情報を隠す', () => {
@@ -32,13 +32,13 @@ describe('独自拡張', () => {
 		});
 
 		describe.each([
-			{ endpoint: '/federation/instances' },
-			{ endpoint: '/federation/show-instance', param: { host: 'misskey.local' }, couldBeEmpty: true },
-			{ endpoint: '/federation/stats' },
-			{ endpoint: '/charts/instance', param: { host: 'misskey.local', span: 'day' } },
-		])('/api$endpoint', ({ endpoint, param, couldBeEmpty }) => {
+			{ endpoint: 'federation/instances' },
+			{ endpoint: 'federation/show-instance', param: { host: 'misskey.local' }, couldBeEmpty: true },
+			{ endpoint: 'federation/stats' },
+			{ endpoint: 'charts/instance', param: { host: 'misskey.local', span: 'day' } },
+		])('/api/$endpoint', ({ endpoint, param, couldBeEmpty }) => {
 			test('はGETできない。', async () => {
-				const res = await simpleGet(`/api${endpoint}`, 'application/json');
+				const res = await simpleGet(`/api/${endpoint}`, 'application/json');
 				assert.strictEqual(res.status, 405);
 			});
 
@@ -62,12 +62,12 @@ describe('独自拡張', () => {
 			});
 
 			test('は認証が必要。', async () => {
-				const res = await api('/get-online-users-count', {});
+				const res = await api('get-online-users-count', {});
 				assert.strictEqual(res.status, 401);
 			});
 
 			test('はトークンがあればアクセスできる。', async () => {
-				const res = await api('/get-online-users-count', {}, bob);
+				const res = await api('get-online-users-count', {}, bob);
 				assert.strictEqual(res.status, 200);
 			});
 		});
@@ -75,34 +75,34 @@ describe('独自拡張', () => {
 
 	describe('各ユーザーのチャートを見られるのを本人とモデレーターのみに', () => {
 		describe.each([
-			{ endpoint: '/charts/user/pv' },
-			{ endpoint: '/charts/user/drive' },
-			{ endpoint: '/charts/user/following' },
-			{ endpoint: '/charts/user/notes' },
-			{ endpoint: '/charts/user/reactions' },
-		])('/api$endpoint', ({ endpoint }) => {
+			{ endpoint: 'charts/user/pv' },
+			{ endpoint: 'charts/user/drive' },
+			{ endpoint: 'charts/user/following' },
+			{ endpoint: 'charts/user/notes' },
+			{ endpoint: 'charts/user/reactions' },
+		])('/api/$endpoint', ({ endpoint }) => {
 			test('はGETできない。', async() => {
-				const res = await simpleGet(`/api${endpoint}`, 'application/json');
+				const res = await simpleGet(`/api/${endpoint}`, 'application/json');
 				assert.strictEqual(res.status, 405);
 			});
 
 			test('は認証が必要。', async () => {
-				const res = await api('/charts/user/pv', {});
+				const res = await api(endpoint, {});
 				assert.strictEqual(res.status, 401);
 			});
 
 			test('はモデレータならアクセスできる。', async () => {
-				const res = await api('/charts/user/pv', { span: 'day', userId: bob.id }, alice);
+				const res = await api(endpoint, { span: 'day', userId: bob.id }, alice);
 				assert.strictEqual(res.status, 200);
 			});
 
 			test('は本人ならアクセスできる。', async () => {
-				const res = await api('/charts/user/pv', { span: 'day', userId: bob.id }, bob);
+				const res = await api(endpoint, { span: 'day', userId: bob.id }, bob);
 				assert.strictEqual(res.status, 200);
 			});
 
 			test('はモデレータでも本人でもないならアクセスできない。', async () => {
-				const res = await api('/charts/user/pv', { span: 'day', userId: alice.id }, bob);
+				const res = await api(endpoint, { span: 'day', userId: alice.id }, bob);
 				assert.strictEqual(res.status, 400);
 			});
 		});
@@ -110,13 +110,13 @@ describe('独自拡張', () => {
 
 	describe('サーバーのチャートを隠す', () => {
 		describe.each([
-			{ endpoint: '/charts/active-users', param: { span: 'day' } },
-			{ endpoint: '/charts/drive', param: { span: 'day' } },
-			{ endpoint: '/charts/notes', param: { span: 'day' } },
-			{ endpoint: '/retention' },
-		])('/api$endpoint', ({ endpoint, param }) => {
+			{ endpoint: 'charts/active-users', param: { span: 'day' } },
+			{ endpoint: 'charts/drive', param: { span: 'day' } },
+			{ endpoint: 'charts/notes', param: { span: 'day' } },
+			{ endpoint: 'retention' },
+		])('/api/$endpoint', ({ endpoint, param }) => {
 			test('はGETできない。', async () => {
-				const res = await simpleGet(`/api${endpoint}`, 'application/json');
+				const res = await simpleGet(`/api/${endpoint}`, 'application/json');
 				assert.strictEqual(res.status, 405);
 			});
 
@@ -134,9 +134,9 @@ describe('独自拡張', () => {
 
 	describe('リモートユーザーのFFの表示をクレデンシャル必須に', () => {
 		describe.each([
-			{ endpoint: '/users/followers' },
-			{ endpoint: '/users/following' },
-		])('/api$endpoint', ({ endpoint }) => {
+			{ endpoint: 'users/followers' },
+			{ endpoint: 'users/following' },
+		])('/api/$endpoint', ({ endpoint }) => {
 			test('は認証が必要。', async () => {
 				const res = await api(endpoint, { userId: remoteUser.id });
 				assert.strictEqual(res.status, 400);
@@ -151,8 +151,8 @@ describe('独自拡張', () => {
 
 	describe('ノート数を隠す', () => {
 		beforeAll(async () => {
-			await api('/notes/create', { text: 'note' }, alice);
-			await api('/notes/create', { text: 'note' }, bob);
+			await api('notes/create', { text: 'note' }, alice);
+			await api('notes/create', { text: 'note' }, bob);
 		});
 
 		describe('/api/stats', () => {
@@ -162,7 +162,7 @@ describe('独自拡張', () => {
 			});
 
 			test('は認証がない場合レスポンスのノート数は0になる。', async () => {
-				const res = await api('/stats', {});
+				const res = await api('stats', {});
 				assert.strictEqual(res.body.notesCount, 0);
 				assert.strictEqual(res.body.originalNotesCount, 0);
 			});
@@ -196,22 +196,22 @@ describe('独自拡張', () => {
 			});
 
 			test('は認証がない場合updatedAtがnullになる。', async () => {
-				const res = await api('/users/show', { userId: alice.id });
+				const res = await api('users/show', { userId: alice.id });
 				assert.strictEqual(res.body.updatedAt, null);
 			});
 
 			test('は認証がない場合notesCountが0になる。', async () => {
-				const res = await api('/users/show', { userId: alice.id });
+				const res = await api('users/show', { userId: alice.id });
 				assert.strictEqual(res.body.notesCount, 0);
 			});
 
 			test('はトークンがあればupdatedAtがnullにならない。', async () => {
-				const res = await api('/users/show', { userId: alice.id }, bob);
+				const res = await api('users/show', { userId: alice.id }, bob);
 				assert.notStrictEqual(res.body.updatedAt, null);
 			});
 
 			test('はトークンがあればnotesCountが0にならない。', async () => {
-				const res = await api('/users/show', { userId: alice.id }, bob);
+				const res = await api('users/show', { userId: alice.id }, bob);
 				assert.notStrictEqual(res.body.notesCount, 0);
 			});
 		});
@@ -225,12 +225,12 @@ describe('独自拡張', () => {
 			});
 
 			test('は認証が必要。', async () => {
-				const res = await api('/notes/featured', {});
+				const res = await api('notes/featured', {});
 				assert.strictEqual(res.status, 401);
 			});
 
 			test('はトークンがあればアクセスできる。', async () => {
-				const res = await api('/notes/featured', {}, bob);
+				const res = await api('notes/featured', {}, bob);
 				assert.strictEqual(res.status, 200);
 			});
 		});
@@ -242,22 +242,22 @@ describe('独自拡張', () => {
 			});
 
 			test('はoriginがlocalの場合認証がなくてもよい。', async () => {
-				const res = await api('/users', { origin: 'local' });
+				const res = await api('users', { origin: 'local' });
 				assert.strictEqual(res.status, 200);
 			});
 
 			test('はoriginがremoteの場合認証が必要。', async () => {
-				const res = await api('/users', { origin: 'remote' });
+				const res = await api('users', { origin: 'remote' });
 				assert.strictEqual(res.status, 400);
 			});
 
 			test('はoriginがcombinedの場合認証が必要。', async () => {
-				const res = await api('/users', { origin: 'combined' });
+				const res = await api('users', { origin: 'combined' });
 				assert.strictEqual(res.status, 400);
 			});
 
 			test('はトークンがあればアクセスできる。', async () => {
-				const res = await api('/users', { origin: 'remote' }, bob);
+				const res = await api('users', { origin: 'remote' }, bob);
 				assert.strictEqual(res.status, 200);
 			});
 		});
@@ -269,15 +269,15 @@ describe('独自拡張', () => {
 		});
 
 		beforeEach(async () => {
-			await api('/following/delete', { userId: remoteUser.id }, alice);
-			await api('/following/delete', { userId: remoteUser.id }, proxy);
+			await api('following/delete', { userId: remoteUser.id }, alice);
+			await api('following/delete', { userId: remoteUser.id }, proxy);
 		});
 
 		test('ローカルにフォロワーのいるリモートユーザーをフォローしてもプロキシアカウントからフォローリクエストが飛ばない。', async () => {
-			await api('/following/create', { userId: remoteUser.id }, alice);
-			const listCreateRes = await api('/users/lists/create', { name: 'list' }, alice);
-			await api('/users/lists/push', { listId: listCreateRes.body.id, userId: remoteUser.id }, alice);
-			const followers = await api('/users/followers', { userId: remoteUser.id }, alice);
+			await api('following/create', { userId: remoteUser.id }, alice);
+			const listCreateRes = await api('users/lists/create', { name: 'list' }, alice);
+			await api('users/lists/push', { listId: listCreateRes.body.id, userId: remoteUser.id }, alice);
+			const followers = await api('users/followers', { userId: remoteUser.id }, alice);
 			assert.strictEqual(followers.body.length, 1);
 			assert.strictEqual(followers.body[0].follower.id, alice.id);
 		});
@@ -295,57 +295,57 @@ describe('独自拡張', () => {
 	describe('通知を除外するミュートの実装', () => {
 		describe('通常のミュート', () => {
 			test('ミュート作成', async () => {
-				const res = await api('/mute/create', { userId: bob.id, expiresAt: null, excludeNotification: false }, alice);
+				const res = await api('mute/create', { userId: bob.id, expiresAt: null, excludeNotification: false }, alice);
 				assert.strictEqual(res.status, 204);
 			});
 
 			test('がTLにミュート対象のユーザーのノートを含まない。', async () => {
-				await api('/notes/create', { text: 'text' }, bob);
-				const res = await api('/notes/timeline', {}, alice);
+				await api('notes/create', { text: 'text' }, bob);
+				const res = await api('notes/timeline', {}, alice);
 				for (const note of res.body) {
 					assert.notStrictEqual(note.userId, bob.id);
 				}
 			});
 
 			test('が通知欄にミュート対象のユーザーからの通知を含まない。', async () => {
-				await api('/notes/create', { text: '@alice text' }, bob);
+				await api('notes/create', { text: '@alice text' }, bob);
 				await sleep(500);
-				const res = await api('/i/notifications', {}, alice);
+				const res = await api('i/notifications', {}, alice);
 				for (const n of res.body) {
 					assert.notStrictEqual(n.userId, bob.id);
 				}
 			});
 
 			test('ミュート解除', async () => {
-				const res = await api('/mute/delete', { userId: bob.id }, alice);
+				const res = await api('mute/delete', { userId: bob.id }, alice);
 				assert.strictEqual(res.status, 204);
 			});
 		});
 
 		describe('通知を除外ずるミュート', () => {
 			test('ミュート作成', async () => {
-				const res = await api('/mute/create', { userId: bob.id, expiresAt: null, excludeNotification: true }, alice);
+				const res = await api('mute/create', { userId: bob.id, expiresAt: null, excludeNotification: true }, alice);
 				assert.strictEqual(res.status, 204);
 			});
 
 			test('がTLにミュート対象のユーザーのノートを含まない。', async () => {
-				await api('/notes/create', { text: 'text' }, bob);
-				const res = await api('/notes/timeline', {}, alice);
+				await api('notes/create', { text: 'text' }, bob);
+				const res = await api('notes/timeline', {}, alice);
 				for (const note of res.body) {
 					assert.notStrictEqual(note.userId, bob.id);
 				}
 			});
 
 			test('が通知欄にミュート対象のユーザーからの通知を含む。', async () => {
-				await api('/notes/create', { text: '@alice text' }, bob);
+				await api('notes/create', { text: '@alice text' }, bob);
 				await sleep(500);
-				const res = await api('/i/notifications', {}, alice);
+				const res = await api('i/notifications', {}, alice);
 				const count = res.body.filter((notification: any) => notification.userId === bob.id).length;
 				assert.notStrictEqual(count, 0);
 			});
 
 			test('ミュート解除', async () => {
-				const res = await api('/mute/delete', { userId: bob.id }, alice);
+				const res = await api('mute/delete', { userId: bob.id }, alice);
 				assert.strictEqual(res.status, 204);
 			});
 		});
