161ef65414d03a572c80bf07db7869ee8f513466
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-10-03 18:18:00 +0900

    feat: サーバー初期設定時に初期パスワードを要求できるように (# 14626)
    * feat: サーバー初期設定時専用の初期パスワードを設定できるように
    
    * 無いのに入力された場合もエラーにする
    
    * :art:
    
    * :art:
    
    * cypress-devcontainerにもpassを設定（テストが失敗するため）
    
    * [ci skip] :art:
    
    * :v:
    
    * test: please revert this commit before merge
    
    * Revert "test: please revert this commit before merge"
    
    This reverts commit 66b2b48f66830d2450d8cda03955c143feba76c7.
    
    * Update locales/ja-JP.yml
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>
    
    * build assets
    
    * Update Changelog
    
    * fix condition
    
    * fix condition
    
    * add comment
    
    * change error code
    
    * 他のエラーコードと合わせる
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: syuilo <4439005+syuilo@users.noreply.github.com>

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -63,6 +63,8 @@ type Source = {
 
 	publishTarballInsteadOfProvideRepositoryUrl?: boolean;
 
+	initialPassword?: string;
+	
 	proxy?: string | { protocol: string, hostname: string, port: number };
 	proxySmtp?: string;
 	proxyBypassHosts?: string[];
@@ -163,6 +165,7 @@ export type Config = {
 
 	version: string;
 	publishTarballInsteadOfProvideRepositoryUrl: boolean;
+	initialPassword: string | undefined;
 	host: string;
 	hostname: string;
 	scheme: string;
@@ -241,6 +244,7 @@ export function loadConfig(): Config {
 	return {
 		version,
 		publishTarballInsteadOfProvideRepositoryUrl: !!config.publishTarballInsteadOfProvideRepositoryUrl,
+		initialPassword: config.initialPassword,
 		url: url.origin,
 		port: config.port ?? parseInt(process.env.PORT ?? '', 10),
 		socket: config.socket,
