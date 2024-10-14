674c251404fcd8db49ba641bf30985d118a3c3ad
syuilo <4439005+syuilo@users.noreply.github.com>
2024-10-14 11:22:02 +0900

    perf(frontend): ノートのレンダリングをスキップできるように

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -379,6 +379,10 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: 'app' as 'app' | 'appWithShift' | 'native',
 	},
+	skipNoteRender: {
+		where: 'device',
+		default: false,
+	},
 
 	sound_masterVolume: {
 		where: 'device',
