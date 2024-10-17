a69bc07b91373589ac08e33f410d26fbaa0d39ff
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-10-17 13:52:10 +0900

    Fix MkCode.renderer stories

--- packages/frontend/src/components/MkCode.renderer.vue
+++ packages/frontend/src/components/MkCode.renderer.vue
@@ -1,5 +1,5 @@
 <template>
-<pre v-if="tokensResult" :class="['shiki', ...(tokensResult.themeName?.split(' ') ?? [])]" :style="tokensResult.rootStyle" tabindex="0"><code><span v-for="line in tokensResult.tokens" class="line"><span v-for="token in line" :style="token.htmlStyle">{{ token.content }}</span>{{ '\n' }}</span></code></pre>
+<pre :class="['shiki', ...(tokensResult.themeName?.split(' ') ?? [])]" :style="tokensResult.rootStyle" tabindex="0"><code><span v-for="line in tokensResult.tokens" class="line"><span v-for="token in line" :style="token.htmlStyle">{{ token.content }}</span>{{ '\n' }}</span></code></pre>
 </template>
 
 <script lang="ts" setup>
