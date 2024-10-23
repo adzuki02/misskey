a4ceba4b722b6886a2ce1c332fd9be30824dbc25
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-10-23 14:45:05 +0900

    add debug logging to inspect e2e failure on actions

--- packages/backend/test/jest.setup.ts
+++ packages/backend/test/jest.setup.ts
@@ -6,6 +6,8 @@
 import { initTestDb, sendEnvResetRequest } from './utils.js';
 
 beforeAll(async () => {
-		await initTestDb(false);
-		await sendEnvResetRequest();
+	await initTestDb(false);
+	console.log('initialized test db');
+	await sendEnvResetRequest();
+	console.log('sent env reset request');
 });
