c4db2423edad5612fe1849e06702ba374b8bc21a
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-10-23 16:44:32 +0900

    Revert "add debug logging to inspect e2e failure on actions"
    This reverts commit a4ceba4b722b6886a2ce1c332fd9be30824dbc25.

--- packages/backend/test/jest.setup.ts
+++ packages/backend/test/jest.setup.ts
@@ -6,8 +6,6 @@
 import { initTestDb, sendEnvResetRequest } from './utils.js';
 
 beforeAll(async () => {
-	await initTestDb(false);
-	console.log('initialized test db');
-	await sendEnvResetRequest();
-	console.log('sent env reset request');
+		await initTestDb(false);
+		await sendEnvResetRequest();
 });
