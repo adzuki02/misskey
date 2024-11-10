9d38fe5ad3f7f7678f1f07d34207eacb3f0974b2
Adzuki <88547072+adzuki02@users.noreply.github.com>
2024-11-10 20:19:52 +0900

    update main.ts

--- packages/frontend/.storybook/main.ts
+++ packages/frontend/.storybook/main.ts
@@ -3,30 +3,25 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { createRequire } from 'node:module';
-import { dirname, join, resolve } from 'node:path';
+import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import type { StorybookConfig } from '@storybook/vue3-vite';
 import { type Plugin, mergeConfig } from 'vite';
 import turbosnap from 'vite-plugin-turbosnap';
 
-const require = createRequire(import.meta.url);
 const _dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	staticDirs: [{ from: '../assets', to: '/client-assets' }],
 	addons: [
-		getAbsolutePath('@storybook/addon-essentials'),
-		getAbsolutePath('@storybook/addon-interactions'),
-		getAbsolutePath('@storybook/addon-links'),
-		getAbsolutePath('@storybook/addon-storysource'),
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-links',
+		'@storybook/addon-storysource',
 		resolve(_dirname, '../node_modules/storybook-addon-misskey-theme'),
 	],
-	framework: {
-		name: getAbsolutePath('@storybook/vue3-vite') as '@storybook/vue3-vite',
-		options: {},
-	},
+	framework: '@storybook/vue3-vite',
 	docs: {
 		autodocs: 'tag',
 	},
@@ -60,7 +55,3 @@ const config: StorybookConfig = {
 };
 
 export default config;
-
-function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, 'package.json')));
-}
